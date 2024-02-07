13943055b1301b8e3459bc8ef2031a16c9b786bf
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2024-02-07 22:12:28 +0100

    Implement LayzVim config

---
+++ lua/config/options.lua
@@ -0,0 +1,11 @@
+local opt = vim.opt
+local g = vim.g
+local o = vim.o
+
+opt.clipboard = ""
+opt.colorcolumn = "80,120"
+opt.mouse = ""
+
+g.neovide_cursor_trail_size = 0.4
+
+o.guifont = "Hack Nerd Font Mono:h12"
---
+++ lua/plugins/core.lua
@@ -0,0 +1,85 @@
+return {
+  -- LSP
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        "hadolint",
+        "golangci-lint",
+        "sqlfluff",
+      })
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        go = { "golangcilint" },
+        sql = { "sqlfluff" },
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "make",
+        "svelte",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        svelte = {},
+      },
+    },
+  },
+
+  -- Tests
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/neotest-plenary",
+      "rouge8/neotest-rust",
+      "nvim-neotest/neotest-go",
+    },
+    opts = {
+      adapters = {
+        "neotest-plenary",
+        "neotest-rust",
+        "neotest-go",
+      },
+    },
+  },
+
+  -- Editor
+  { "echasnovski/mini.indentscope", enabled = false },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    opts = {
+      scope = { enabled = true },
+    },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+    },
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {},
+    },
+  },
+
+  -- Languages
+  -- TODO: Delete when will be available just in "nvim-treesitter/nvim-treesitter"
+  {
+    "NoahTheDuke/vim-just",
+    ft = { "just" },
+  },
+}
