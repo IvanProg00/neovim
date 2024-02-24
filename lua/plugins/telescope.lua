fc47df3387f5a4c0da70fc71238190788ceb9d76
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2024-02-24 10:16:17 +0100

    Change keymaps for telescope to `cycle_history_*`

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,15 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  opts = {
-    defaults = {
-      layout_config = {
-        prompt_position = "top",
-      },
-      sorting_strategy = "ascending",
-    },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function(_, opts)
+      local actions = require("telescope.actions")
+
+      opts.defaults.layout_config = { prompt_position = "top" }
+      opts.defaults.sorting_strategy = "ascending"
+      opts.defaults.mappings.i["<C-k>"] = actions.cycle_history_prev
+      opts.defaults.mappings.i["<C-j>"] = actions.cycle_history_next
+      opts.defaults.mappings.i["<C-Up>"] = nil
+      opts.defaults.mappings.i["<C-Down>"] = nil
+    end,
   },
 }
