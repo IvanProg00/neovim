ef75ec87159466532a1bb38c3ac795e5c3267bb9
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2024-06-11 09:00:28 +0200

    Add extra `git`

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.nushell" },
