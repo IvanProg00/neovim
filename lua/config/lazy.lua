1aea17bfca295f1853a87534ceaf77f3b8137c63
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2024-07-26 18:16:07 +0200

    Delete `luasnip` dependency

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,7 +20,6 @@ require("lazy").setup({
     },
 
     { import = "lazyvim.plugins.extras.coding.codeium" },
-    { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
 
     { import = "lazyvim.plugins.extras.editor.navic" },
